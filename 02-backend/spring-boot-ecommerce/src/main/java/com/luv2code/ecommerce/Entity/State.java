a9f2b54fe8f18365dfd7885284f2dd2412c0ef88
viego <viego@nyu.com>
2025-03-29 01:26:42 -0400

    update project

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/Entity/State.java
@@ -0,0 +1,26 @@
+package com.luv2code.ecommerce.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name="state")
+@Getter
+@Setter
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="country_id")
+    private Country country;
+}
